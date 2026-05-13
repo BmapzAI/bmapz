3aae463a907ffe8a4dc9858b8073025ed0e4ce2f
BmapzAI <d2mdigitalmarketing@gmail.com>
2026-05-13 18:44:59 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    content: ["./index.html", "./frontend-src/**/*.{ts,tsx,js,jsx}"],
     safelist: [
       'md:ml-[72px]', 'md:ml-[240px]',
       'ml-[72px]', 'ml-[240px]',
